43d8f33325cec94386fc5b03dbc3b646f66ae203
WanderingLemon <wisdomcr9@gmail.com>
2023-08-09 23:27:15 -0400

    added the ratatui template

---
+++ src/app.rs
@@ -0,0 +1,42 @@
+use std::error;
+
+/// Application result type.
+pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+/// Application.
+#[derive(Debug)]
+pub struct App {
+    /// Is the application running?
+    pub running: bool,
+
+
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            running: true,
+            /*
+            What needs to be kept track of?
+            Files that are to be displayed and all of their information => put this into a struct
+            Current dir
+            What file is selected
+             */
+        }
+    }
+}
+
+impl App {
+    /// Constructs a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Handles the tick event of the terminal.
+    pub fn tick(&self) {}
+
+    /// Set running to false to quit the application.
+    pub fn quit(&mut self) {
+        self.running = false;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
-mod util;
+use lemon_files::app::{App, AppResult};
+use lemon_files::event::{Event, EventHandler};
+use lemon_files::handler::handle_key_events;
+use lemon_files::tui::Tui;
+use std::io;
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
 
-use std::fs;
-use std::os::unix::prelude::{PermissionsExt};
+fn main() -> AppResult<()> {
+    // Create an application.
+    let mut app = App::new();
 
-fn main() {
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
 
-    let dir = fs::read_dir(".").unwrap();
-    for entry in dir {
-        let entry = entry.unwrap();
-        let meta = entry.metadata().unwrap();
-        let permissions = meta.permissions().mode();
-
-        println!("{}", String::from(entry.path().to_string_lossy().strip_prefix("./").unwrap()));
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next()? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
     }
+
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
 }
+
+// }fn main() {
+//
+//     let dir = fs::read_dir(".").unwrap();
+//     for entry in dir {
+//         let entry = entry.unwrap();
+//         let meta = entry.metadata().unwrap();
+//         let permissions = meta.permissions().mode();
+//
+//         println!("{}", String::from(entry.path().to_string_lossy().strip_prefix("./").unwrap()));
+//     }
+// }
